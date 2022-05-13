4a37a930f8847a13975f5b7f96acd7e8c77099ed
syrena <449199948@qq.com>
2022-05-13 11:44:21 +0800

    no message

---
+++ SwiftUI_study/SwiftUI_study/SwiftUI_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_studyApp.swift
+//  SwiftUI_study
+//
+//  Created by apple on 2022/5/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
